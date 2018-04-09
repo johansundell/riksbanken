5532a7eafde5dda7b377bd8e717c69836ca95448
Johan Sundell <johan@pixpro.net>
2018-04-09 11:13:12 +0200

    Moved some code

--- main.go
+++ main.go
@@ -1,95 +1,11 @@
 package main
 
 import (
-	"bytes"
-	"encoding/xml"
-	"fmt"
-	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"strings"
 )
-
-type Envelope struct {
-	XMLName xml.Name `xml:"Envelope"`
-	Body    struct {
-		XMLName      xml.Name `xml:"Body"`
-		ExchangeRate struct {
-			XMLName xml.Name `xml:"getLatestInterestAndExchangeRatesResponse"`
-			Groups  struct {
-				//XMLName xml.Name `xml:"return>groups"`
-				GroupId   string `xml:"groupid"`
-				GroupName string `xml:"groupname"`
-				Series    []struct {
-					SeriesId   string  `xml:"seriesid"`
-					SeriesName string  `xml:"seriesname"`
-					Unit       float32 `xml:"unit"`
-					Value      float32 `xml:"resultrows>value"`
-				} `xml:"series"`
-			} `xml:"return>groups"`
-		}
-	}
-}
-
-const getEnvelope = `<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns:xsd="http://swea.riksbank.se/xsd">
-	<soap:Body>
-	<xsd:getLatestInterestAndExchangeRates>
-        	<languageid>en</languageid>
-        	<seriesid>{{.CurrencyCode}}</seriesid>
-      </xsd:getLatestInterestAndExchangeRates>   
-      </soap:Body>
-</soap:Envelope>`
 
 func main() {
 	http.HandleFunc("/", MainHandler)
 	log.Println(http.ListenAndServe(":8080", nil))
-}
-
-func MainHandler(w http.ResponseWriter, r *http.Request) {
-	rate, err := getCurrencyRate("SEKEURPMI")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	s := fmt.Sprintf("%f", rate)
-	fmt.Fprintf(w, strings.Replace(s, ".", ",", -1))
-}
-
-func getCurrencyRate(currencyCode string) (float32, error) {
-	tmpl, err := template.New("getEnvelope").Parse(getEnvelope)
-	if err != nil {
-		return 0, err
-	}
-	var docs bytes.Buffer
-	type CurrencyData struct {
-		CurrencyCode string
-	}
-	currency := CurrencyData{CurrencyCode: currencyCode}
-	if err := tmpl.Execute(&docs, currency); err != nil {
-		return 0, err
-	}
-	url := "https://swea.riksbank.se:443/sweaWS/services/SweaWebServiceHttpSoap12Endpoint"
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", url, &docs)
-	if err != nil {
-		return 0, err
-	}
-	req.Header.Add("Content-Type", "application/xml")
-	resp, err := client.Do(req)
-	if err != nil {
-		return 0, err
-	}
-	defer resp.Body.Close()
-	contents, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-	//fmt.Println(string(contents))
-
-	test := Envelope{}
-	if err := xml.Unmarshal(contents, &test); err != nil {
-		return 0, err
-	}
-	return test.Body.ExchangeRate.Groups.Series[0].Value, nil
 }
